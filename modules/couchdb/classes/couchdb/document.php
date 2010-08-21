b2df657567dca7118e0d4d8b408495dac8e9b621
Shea Rial <sheatrevor@gmail.com>
2010-08-21 19:51:48 -0400

    Work.

--- modules/couchdb/classes/couchdb/document.php
+++ modules/couchdb/classes/couchdb/document.php
@@ -12,14 +12,19 @@
 class CouchDB_Document {
 
 	/**
-	 * @var  string  default CouchDB configuration group
+	 * @var  string  default configuration group
 	 */
 	public static $default = 'default';
 
 	/**
-	 * @var  array  holds the name of the CouchDB configuration group this document belongs to
+	 * @var  array  holds the name of the configuration group this document belongs to
 	 */
 	protected $_group;
+
+	/**
+	 * @var  boolean  holds if this document is new or not
+	 */
+	protected $_new;
 
 	/**
 	 * @var  string  holds the document id
@@ -37,39 +42,94 @@ class CouchDB_Document {
 	protected $_data;
 
 	/**
-	 * Manages a single CouchDB document. The document 
+	 * Manages a single document
 	 *
-	 * @param   string  the document id that we should load, if assigned
-	 * @param   string  the name of the CouchDB configuration group this document belongs to
+	 * @param   string  if set, the document id that we should load
+	 * @param   string  the name of the configuration group this document should belong to
 	 * @return  void
 	 */
-	public function __construct($name = NULL, $id = NULL)
+	public function __construct($id = NULL, $group = NULL)
 	{
-		// If no group name was passed in
-		if ($name === NULL)
+		if ($group === NULL)
 		{
 			// Use the default group name
-			$name = self::$default;
+			$group = self::$default;
 		}
 
-		// Get the configuration data for this group
-		$config = Kohana::config('couchdb')->$name;
+		// Store the name of the configuration group
+		$this->_group = $group;
 
-		// Store a reference to the configuration data locally
-		$this->_config = $config;
+		// If we were given a document id
+		if ($id !== NULL)
+		{
+			// Attempt to load this document
+			$this->load($id);
+		}
 	}
 
 	/**
-	 * Gets the document that was requested, caches its information locally, and returns it
+	 * Returns a single named member of this document
 	 *
-	 * @param   string  the document id that we are requesting
+	 * @param   string  the name of the member we are requesting
 	 * @return  mixed   the parsed document
 	 */
-	public function __get($id)
+	public function __get($name)
 	{
+		// Return the member name from the data
+		return $this->_data->$name;
 	}
 
 	/**
-	 * Saves any changes have been made to this document if any changes have been made at all
+	 * Attempts to load this document
+	 *
+	 * @return  mixed  a reference to this class instance
+	 */
+	protected function _load($id)
+	{
+		// The call to load will fail if the document is new
+		try {
+
+			// Attempt to load the document data
+			$this->_data = CouchDB_Client::instance($this->_group)->$id;
+
+		// Catch all exceptions
+		} catch ($exception) {
+
+			// If the exception indicates that the document is unavailable
+			if ($exception instanceof CouchDB_Document_Unavailable_Exception)
+			{
+				// Mark this document as new
+				$this->_new = TRUE;
+			}
+			else
+			{
+				// We arent sure what to do, so re-throw the exception
+				throw $exception;
+			}
+
+		}
+
+		// Return a reference to this class instance
+		return $this;
+	}
+
+	/**
+	 * Reloads the 
+
+	/**
+	 * Saves any changes that were made to this document
+	 *
+	 * @param   string  if set, the document id that we should use to save the document
+	 * @param   string  the name of the configuration group this document should belong to
+	 * @return  object  a reference to this class instance
+	 */
+	public function save($id = NULL, $group = NULL)
+	{
+		if ($group === NULL)
+		{
+			// Use the group name that was set in the constructor
+			$group = $this->_group;
+		}
+	}
 
 }
